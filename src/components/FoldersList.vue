5f86058911144a8fe56aa6db72c80bc511313df8
Expant <anton532202@gmail.com>
2025-02-03 23:01:12 +0300

    learning to work with chrome api

--- src/components/FoldersList.vue
+++ src/components/FoldersList.vue
@@ -1,8 +1,35 @@
+<script setup>
+import { onMounted } from 'vue';
+
+
+// const addNewFolder = () => {
+  
+// };
+
+onMounted(() => {
+  chrome.storage.local.set({ 'frontend': { type: 'folder' } }, () => {
+    message.value = "Данные сохранены";
+  });
+  chrome.storage.local.set({ 'backend': { type: 'folder' } }, () => {
+    message.value = "Данные сохранены";
+  });
+
+  console.log(chrome.storage.local.get(null, (items) => {
+    const keys = Object.keys(items).forEach((key) => {
+      chrome.storage.local.get([key], (result) => console.log(result));
+    });
+    console.log(keys);
+  }));
+})
+
+
+</script>
+
 <template>
   <div class="folders">
-    <button class="new-folder">New folder</button>
-    <ul class="folders__list"></ul>
+    <button @click="addNewFolder" class="new-folder">New folder</button>
+    <ul class="folders__list">
+      
+    </ul>
   </div>
 </template>
-
-<script setup></script>
