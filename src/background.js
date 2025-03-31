e1ef714e9c9eddf53852b49f37a1748afa4f7b15
Expant <anton532202@gmail.com>
2025-03-31 23:09:32 +0300

    add clearStorage

--- src/background.js
+++ src/background.js
@@ -4,6 +4,7 @@ const apiPath = "/api/v0/chat_session";
 const trackСhatDeletion = async (details) => {
   const { method, url } = details;
   const isDelete = method === "POST" && url.includes(`${apiPath}/delete`);
+  
   if (!isDelete) return;
 
   const [activeTab] = await chrome.tabs.query({
@@ -14,8 +15,14 @@ const trackСhatDeletion = async (details) => {
   if (activeTab) {
     const args = [activeTab.id, { action: "chatDeleted" }];
     await chrome.tabs.sendMessage(...args);
-    console.log("Сообщение отправлено:");
   }
 };
 
+const clearStorage = (details) => {
+  if (details.reason === 'uninstall') {
+    chrome.storage.sync.clear();
+  }
+}
+
 chrome.webRequest.onCompleted.addListener(trackСhatDeletion, { urls });
+chrome.runtime.onInstalled.addListener(clearStorage);
